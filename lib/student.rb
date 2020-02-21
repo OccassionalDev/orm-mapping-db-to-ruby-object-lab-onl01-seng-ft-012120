39c344f108ffca89aa1585be116435e6d3c1da83
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-21 02:32:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,5 +74,15 @@ class Student
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end 
+  end
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT COUNT(*) FROM students WHERE grade < 12
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db
+    end 
   end 
 end
